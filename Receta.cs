f1c78b2fc902948fbfaa2d8054ca83cdac6d94da
d-mich-j <159874041+d-mich-j@users.noreply.github.com>
2026-05-13 10:57:26 -0600

    Refactor Receta class and add interface implementation

--- Receta.cs
+++ Receta.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using SistemaRecetas.Interfaces;
 
-public class Receta 
+namespace SistemaRecetas.Modelos;
+
+public class Receta : IReceta
 {
     public string Nombre { get; }
     public string Chef { get; }
     public int TiempoMinutos { get; }
-    
 
-    public Receta(string nombre, string chef, int tiempoMinuto) {
+    public Receta(string nombre, string chef, int tiempoMinutos)
+    {
+        if (tiempoMinutos <= 0)
+            throw new ArgumentException("El tiempo de preparación debe ser mayor que 0.", nameof(tiempoMinutos));
+
         Nombre = nombre;
         Chef = chef;
-        //...
+        TiempoMinutos = tiempoMinutos;
     }
-    
+
+    public override string ToString()
+    {
+        return $"{Nombre} - {Chef} ({TiempoMinutos} min)";
+    }
 }
