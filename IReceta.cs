bee75e8f0d7bad7edcc952835365997cc440fd82
YahirToledo <yahir.toledo.r@gmail.com>
2026-05-13 10:27:32 -0600

    cambio

---
+++ IReceta.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaRecetas.Interfaces;
+
+public interface IReceta {
+    string Nombre { get; }
+    string Chef { get; }
+    int TiempoMinutos { get; }
+    string ToString();
+}
+	public Class1()
+	{
+	}
+}
